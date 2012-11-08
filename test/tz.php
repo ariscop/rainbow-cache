2b38f71bedce797061e32edf8c2a8f8aedd4e030
Andrew Cook <ariscop@gmail.com>
2012-11-08 18:39:34 +1100

    display microtime on tz.php, also add microtime output

--- test/tz.php
+++ test/tz.php
@@ -4,6 +4,7 @@
 	</head>
 	<body>
 		Utc time from php: <?php echo date('YmdHis', microtime(true)); ?></br>
-		Local time from apache: <?php echo $_SERVER['REDIRECT_A_TIME']; ?></br>		
+		Local time from apache: <?php echo $_SERVER['REDIRECT_A_TIME'], $_SERVER['A_TIME']; ?></br>
+		Current value of microtime(true): <?php var_dump(microtime(true)); ?></br>
 	</body>
 </html>
